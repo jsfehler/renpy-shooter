401d1951c1384f2046ec784d7818d856545b9756
Joshua Fehler <jsfehler@gmail.com>
2018-03-01 18:02:09 -0500

    Code style fixes

--- actors.py
+++ actors.py
@@ -1,20 +1,30 @@
 import pygame
+import renpy.exports as renpy
+
 
 class ShooterActor(renpy.Displayable):
     """Base class for Shooter objects.
     """
-    def __init__(self, displayable=None, speed=(0, 0), start=(0, 0), *args, **kwargs):
-        super(ShooterActor, self).__init__(**kwargs)
+    solid_colour = "#ffffff"
+    solid_kwargs = {
+        "xsize": 20,
+        "ysize": 20
+    }
+
+    def __init__(self, displayable=None, speed=(0, 0), start=(0, 0),
+                 *args, **kwargs):
+        super(ShooterActor, self).__init__(*args, **kwargs)
 
         # Stores the old value of the st value from render()
         self.old_st = None
 
         # Default displayable if none is provided.
-        self.displayable = displayable or Solid("#ffffff", xsize=20, ysize=20)
+        self.displayable = displayable or Solid(
+            self.solid_colour, **self.solid_kwargs)
 
         self.speed_x = 0
         self.speed_y = 0
-        
+
         self.max_speed_x = speed[0]
         self.max_speed_y = speed[1]
 
@@ -23,14 +33,16 @@ class ShooterActor(renpy.Displayable):
 
 
 class ShooterPlayer(ShooterActor):
-    def __init__(self, displayable=None, speed=(0, 0), start=(0, 0), *args, **kwargs):
-        super(ShooterPlayer, self).__init__(displayable, speed, start, **kwargs)    
+    def __init__(self, displayable=None, speed=(0, 0), start=(0, 0),
+                 *args, **kwargs):
+        super(ShooterPlayer, self).__init__(
+            displayable, speed, start, *args, **kwargs)
 
         self.bullets = []
         self.max_bullets = 6
 
         self.enemies = []
-        
+
     def check_overlap(self):
         pass
 
@@ -42,9 +54,10 @@ class ShooterPlayer(ShooterActor):
             if not item.alive:
                 self.bullets.remove(item)
 
-        # If there are less than the max number of bullets allowed, add a new bullet
+        # If less than the max number of bullets allowed, add a new bullet
         if len(self.bullets) < (self.max_bullets - 1):
-            self.bullets.append(ShooterBullet(start=(self.x, self.y), speed=(0, 300)))
+            new_bullet = ShooterBullet(start=(self.x, self.y), speed=(0, 300))
+            self.bullets.append(new_bullet)
 
     def move_player(self):
         keys = pygame.key.get_pressed()
@@ -74,9 +87,6 @@ class ShooterPlayer(ShooterActor):
         speed_x = dtime * self.speed_x
         speed_y = dtime * self.speed_y
 
-        old_x = self.x          
-        old_y = self.y
-
         self.x += speed_x
         self.y += speed_y
 
@@ -94,7 +104,6 @@ class ShooterPlayer(ShooterActor):
         debug_render = renpy.render(t, width, height, st, at)
         render.blit(debug_render, (0, 0))
 
-
         return render
 
     def event(self, ev, x, y, st):
@@ -105,24 +114,18 @@ class ShooterPlayer(ShooterActor):
         elif ev.type == pygame.KEYDOWN:
             if ev.key == pygame.K_SPACE:
                 self.generate_bullets()
-                
+
 
 class ShooterBullet(renpy.Displayable):
+
+    solid_colour = "#ff33ee"
+    solid_kwargs = {
+        "xsize": 20,
+        "ysize": 20
+    }
+
     def __init__(self, displayable=None, speed=(0, 0), start=(0, 0), *args, **kwargs):
-        super(ShooterBullet, self).__init__(**kwargs)
-
-        self.old_st = None
-
-        self.displayable = displayable or Solid("#ff33ee", xsize=10, ysize=10)
-
-        self.speed_x = speed[0]
-        self.speed_y = speed[1]
-
-        self.start_x = start[0]
-        self.start_y = start[1]
-
-        self.x = start[0]
-        self.y = start[1]            
+        super(ShooterBullet, self).__init__(*args, **kwargs)
 
         self.alive = True
 
@@ -139,9 +142,6 @@ class ShooterBullet(renpy.Displayable):
         speed_x = dtime * self.speed_x
         speed_y = dtime * self.speed_y
 
-        old_x = self.x          
-        old_y = self.y
-
         self.x -= speed_x
         self.y -= speed_y
 
@@ -155,6 +155,7 @@ class ShooterBullet(renpy.Displayable):
             self.alive = False
 
         return render
+
 
 class EnemyShooterActor(ShooterActor):
     def __init__(self, *args, **kwargs):
@@ -182,14 +183,10 @@ class EnemyShooterActor(ShooterActor):
             foo = -150
         if self.wave <= -1:
             self.wave = 1
-            foo = 0            
-
+            foo = 0
 
         speed_x = dtime * (self.max_speed_x + foo)
         speed_y = dtime * self.max_speed_y
-
-        old_x = self.x          
-        old_y = self.y
 
         self.x += speed_x
         self.y += speed_y
--- script.rpy
+++ script.rpy
@@ -4,13 +4,12 @@ default enemy1 = EnemyShooterActor(speed=(0, 150), start=(0, 0))
 default enemy2 = EnemyShooterActor(speed=(0, 150), start=(0, -50))
 
 default enemy_group = EnemyGroup(start=(400, 0), enemies=[enemy1, enemy2])
-    
-screen shooter():    
+
+screen shooter_main():
     add actor
-    
+
     for enemy in enemy_group.enemies:
         add enemy
-    
+
 label start:
-    
-    call screen shooter
+    call screen shooter_main
